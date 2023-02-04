9e913f398412318c9619ac920ae9636343fb74b4
BatyrDaurov <batyr.daurov@mail.ru>
2023-02-05 00:13:38 +0300

    refactor: add _document.tsx in pages

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ru">
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <Script
+            strategy="afterInteractive"
+            src="//m.servedby-buysellads.com/monetization.js"
+            type="text/javascript"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from 'next';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 
 const Home: NextPage = () => {
-  return <PrimaryLayout>App</PrimaryLayout>;
+  return (
+    <>
+      <PrimaryLayout>App</PrimaryLayout>
+    </>
+  );
 };
 
 export default Home;
