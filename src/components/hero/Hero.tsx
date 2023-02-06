309981e256337f9fcc31ee838e26e67197ae2d05
BatyrDaurov <batyr.daurov@mail.ru>
2023-02-06 09:40:41 +0300

    style: stylize hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -9,7 +9,9 @@ const Hero = (props: Props) => {
   return (
     <section className={s.hero}>
       <div className={s.hero__info}>
-        <h1 className={s.hero__title}>Hi, i’m Batyr Front end dev</h1>
+        <h1 className={s.hero__title}>
+          Hi, i’m Batyr <br /> Front end dev
+        </h1>
         <p className={s.hero__text}>
           On this blog I share tips and tricks, frameworks, projects, tutorials,
           etc Make sure you subscribe to get the latest updates
@@ -20,7 +22,12 @@ const Hero = (props: Props) => {
         </form>
       </div>
       <div className={s.hero__banner}>
-        <Image src="hero.svg" width={370} height={435} alt="hero" />
+        <Image
+          src="hero.svg"
+          width={370}
+          height={435}
+          alt="Hero Banner | Batyr.blog"
+        />
       </div>
     </section>
   );
