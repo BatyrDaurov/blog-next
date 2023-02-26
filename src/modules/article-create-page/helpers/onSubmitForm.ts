12fa155675022c4f30069a86596761c97baeada4
BatyrDaurov <118624947+BatyrDaurov@users.noreply.github.com>
2023-02-26 18:19:19 +0300

    fix: type error impot

--- src/modules/article-create-page/helpers/onSubmitForm.ts
+++ src/modules/article-create-page/helpers/onSubmitForm.ts
@@ -1,17 +1,17 @@
 import { NextRouter } from 'next/router';
 import api from '../../../../axios.config';
-import { ArticleStateType } from '../types';
+import { ArticleStateType } from '../types/ArticleStateType';
 
 export const onSubmit =
-  (article: ArticleStateType, token: string, router: NextRouter) => async () => {
+  (article: ArticleStateType, token: string, router: NextRouter) =>
+  async () => {
     try {
       const { data } = await api.post(
         `${process.env.NEXT_PUBLIC_API_URL}articles`,
         article,
         {
           headers: {
-            Authorization:
-              token,
+            Authorization: token,
           },
         }
       );
